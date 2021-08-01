ed4e990b2acda4c5ab2967adaf3431d511c56304
Robert Hardy <hardyrar@gmail.com>
2021-08-01 19:16:05 +0100

    Create a comment on a given post

---
+++ client/src/components/CommentCreate.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+
+    setContent(""); // Revert input to empty once submitted
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className={"form-group"}>
+          <label>New comment</label>
+          <input
+            className={"form-control"}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <button className={"btn btn-primary"}>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentCreate;
