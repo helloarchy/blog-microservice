174fed0f92ac1fda29f2cdb6360c61e1f3bbb860
Robert Hardy <hardyrar@gmail.com>
2021-08-01 19:01:58 +0100

    Fetch posts and display them

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import PostCreate from "./components/PostCreate";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <div className="container">
       <h1>Create post</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 }
