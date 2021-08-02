cb57a62104780e6d869419510c00856b88607c23
Robert Hardy <hardyrar@gmail.com>
2021-08-02 10:26:26 +0100

    Emit event on post creation

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import CommentCreate from "./CommentCreate";
 
 const CommentList = ({ postId }) => {
   const [comments, setComments] = useState([]);
