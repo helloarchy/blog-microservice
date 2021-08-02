6f3f3364b30376990b0d3456f555231bad6f227d
Robert Hardy <hardyrar@gmail.com>
2021-08-02 10:33:04 +0100

    Emit event on comment creation

--- comments/index.js
+++ comments/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const {randomBytes} = require("crypto");
+const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 // Set up middleware
 const app = express();
@@ -12,24 +13,35 @@ const commentsByPostId = {};
 
 // Get all comments for a given post
 app.get("/posts/:id/comments", (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 // Create a new comment for a give post
-app.post("/posts/:id/comments", (req, res) => {
-    const commentId = randomBytes(4).toString("hex");
-    const { content } = req.body;
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const postId = req.params.id;
 
-    const comments = commentsByPostId[req.params.id] || [];
+  const comments = commentsByPostId[postId] || [];
 
-    comments.push({id: commentId, content});
+  comments.push({ id: commentId, content });
 
-    commentsByPostId[req.params.id] = comments;
+  commentsByPostId[postId] = comments;
 
-    // Send back all comment on the post
-    res.status(201).send(comments);
+  // Emit event now comment created
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      postId,
+      content,
+    },
+  });
+
+  // Send back all comment on the post
+  res.status(201).send(comments);
 });
 
 app.listen(4001, () => {
-    console.log('Listening on port 4001');
+  console.log("Listening on port 4001");
 });
