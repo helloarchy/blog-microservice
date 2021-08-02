ece35a3d39731096a41b64589ce633b228d345b5
Robert Hardy <hardyrar@gmail.com>
2021-08-02 20:20:13 +0100

    Moderate a given comment

--- moderation/index.js
+++ moderation/index.js
@@ -6,10 +6,27 @@ const app = express();
 app.use(bodyParser.json());
 
 // Moderate a given comment
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
     console.log("Event received", req.body)
 
+    const {type, data} = req.body;
+    const forbidden = "orange"
 
+    if (type === "CommentCreated") {
+        // Moderate new comment
+        const status = data.content.status.contains(forbidden) ? "rejected" : "approved"
+
+        // Emit event
+        await axios.post("http://localhost:4005/events", {
+            type: "CommentModerated",
+            content: {
+                id: data.id,
+                content: data.content,
+                postId: data.postId,
+                status,
+            }
+        })
+    }
 })
 
 app.listen(4003, () => {
