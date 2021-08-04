5da4fe67e384b232f627dcbe6616ad62aac04256
Robert Hardy <hardyrar@gmail.com>
2021-08-04 18:53:45 +0100

    Update endpoints for ingress

--- posts/index.js
+++ posts/index.js
@@ -17,7 +17,7 @@ app.get("/posts", (req, res) => {
 });
 
 // Create a post
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
